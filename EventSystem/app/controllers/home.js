25ed82d8a7e25a7042a1327fe47a12516aeaaa53
shunobaka <vesselin_minev@abv.bg>
2016-01-10 14:49:33 +0200

    Fixed nodejs server error

--- EventSystem/app/controllers/home.js
+++ EventSystem/app/controllers/home.js
@@ -1,14 +1,15 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+  Event = mongoose.model('Event');
+//Article = mongoose.model('Article');
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
 router.get('/', function (req, res, next) {
-  Article.find(function (err, articles) {
+  Event.find(function (err, articles) {
     if (err) return next(err);
     res.render('index', {
       title: 'Generator-Express MVC',
