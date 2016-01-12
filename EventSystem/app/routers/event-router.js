c59c2b06750001f724a873778670b117ffac1c02
shunobaka <vesselin_minev@abv.bg>
2016-01-12 20:05:27 +0200

    Extracted routers into separate folder

---
+++ EventSystem/app/routers/event-router.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var express = require('express'),
+  router = new express.Router(),
+  controllers = require('../controllers');
+
+/* Events */
+// --- PUBLIC (ALL) ---
+
+// GET events - Gets top 10 public events, sorted by their date of creation
+router.get('/', controllers.events.getAllEvents);
+
+////GET events/{eventId} - Gets event (details) with Id = eventId, with 10 comments sorted by date
+router.get('/:id', controllers.events.getEventById);
+//
+// //GET events?page=page (*P) - Gets the events at positions from P10 to (P+1)10. The events sorted by date of creation and are at most 10.
+// // TODO: app.get('/:id', controllers.events.getEvents);
+//
+////POST events/{eventID}/comments - Creates a new comment for a given event
+//app.post('/:id/comments', controllers.comments.postComment);
+//
+////POST events/{eventId}/rate header body: { “rate”: 1-5 (int)} - Creates rating by the current user for the event with Id = eventId
+//app.post('/:id/rate', controllers.ratings.rateEvent);
+//
+////POST events/{eventID}/join - Current user can join to the event with Id = eventId
+//app.post('/:id/join', controllers.events.joinEvent);
+//
+// //POST events/{eventID}/leave - Current user can leave an event with ID = eventId
+//app.post('/:id/leave', controllers.events.leaveEvent);
+//
+//// --- ADMIN ONLY ---
+//POST events - Creates a new event and redirects to the newly created event
+// TODO: auth.isInRole('admin'),
+router.post('/', controllers.events.createEvent);
+
+//
+////PUT events/{eventId} - Update an existing event, redirects to events
+//app.put('/:id', auth.isInRole('admin'), controllers.events.updateEventById);
+//
+////DELETE events/{eventId} - Delete an existing event, returns the event created so it can be loaded in the UI
+//app.delete('/:id', auth.isInRole('admin'), controllers.events.deleteEvent);
+
+
+module.exports = function(app) {
+  app.use('/events', router);
+}
---
+++ EventSystem/app/routers/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var fs = require('fs'),
+  path = require('path');
+
+module.exports = function(app) {
+  fs.readdirSync(__dirname)
+    .filter(file => file.indexOf('-router') >= 0)
+    .forEach(file => require(path.join(__dirname, file))(app));
+
+  app.get('/*', function(req, res) {
+    res.render('error', {
+        message: 'The page you are looking for could not be found!',
+        error: {
+          status: "Status: Error 404",
+          stack: "Stack not provided!"
+        }
+      }
+    );
+  });
+
+  app.get('*', function(req, res) {
+    res.render('index', {currentUser: req.user});
+  });
+};
