f81f3e517e73c2adcaa203ad530952fb7b485373
georgimanov <georgimanov@gmail.com>
2016-01-13 16:50:35 +0200

    adding comments implemented; route comments/all implemented

--- EventSystem/app/controllers/commentsController.js
+++ EventSystem/app/controllers/commentsController.js
@@ -1,4 +1,5 @@
 var Comment = require('mongoose').model('Comment');
+var Event = require('mongoose').model('Event');
 
 module.exports = {
   getCommentCreationForm: function(req, res){
@@ -8,24 +9,46 @@ module.exports = {
       eventId: req.params.id
     });
   },
+  getAll: function(req, res) {
+    Comment.find({}).exec(function(err, commentsList) {
+      if (err) {
+        console.log('Failed to get event: ' + err);
+        return;
+      }
+      res.render('comment/list-comments', {
+        title: 'Comments list:',
+        user: req.user,
+        comments: commentsList,
+      });
+    });
+  },
   postComment: function(req, res) {
-    var newPost = {};
-    newPost = {
+    var currentEventId = req.body.eventId;
+
+    var newComment = {};
+    newComment = {
       content: req.body.content,
-      userId: req.body.userId,
-      eventId: req.body.eventId
+      userId: req.user.id,
+      eventId: currentEventId
     };
 
-    Comment.create(newPost, function(err, createdComment){
+    Comment.create(newComment, function(err, createdComment){
       if (err) {
         console.log('Failed to create new comments: ' + err);
         return;
       }
 
-      res.redirect('events/' + eventId,{
-        title: 'New event',
-        user: req.user,
-        comment: createdComment
+      console.log('commentAdded');
+
+      Event.findById(currentEventId).exec(function(err, event) {
+        if(err){
+          console.log('Failed to get event: ' + err);
+          return;
+        }
+        event.comments.push(createdComment);
+        event.save();
       });
-  });
+
+      res.redirect('/events/' + currentEventId);
+    });
 }};
--- EventSystem/app/routers/comment-router.js
+++ EventSystem/app/routers/comment-router.js
@@ -4,8 +4,11 @@ var express = require('express'),
   router = new express.Router(),
   controllers = require('../controllers');
 
+router.get('/all', controllers.comments.getAll);
+
 //POST events/{eventID}/comments - Creates a new comment for a given event
 router.post('/', controllers.comments.postComment);
+
 
 module.exports = function(app) {
   app.use('/comments', router);
