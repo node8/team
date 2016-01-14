b1deeab0e970ecda0f8a48f6d696ddbcd7238d54
shunobaka <vesselin_minev@abv.bg>
2016-01-14 12:02:50 +0200

    updated auth to redirect unauthorized users

--- EventSystem/config/auth.js
+++ EventSystem/config/auth.js
@@ -1,43 +1,47 @@
 var passport = require('passport');
 
 module.exports = {
-    login: function(req, res, next) {
-        var auth = passport.authenticate('local', function(err, user) {
-            if (err) return next(err);
-            if (!user) {
-                res.send({success: false})
-            }
+  login: function (req, res, next) {
+    var auth = passport.authenticate('local', function (err, user) {
+      if (err) return next(err);
+      if (!user) {
+        res.send({success: false})
+      }
 
-            req.logIn(user, function(err) {
-                if (err) return next(err);
-                res.send({success: true, user: user});
-            })
-        });
+      req.logIn(user, function (err) {
+        if (err) return next(err);
+        res.send({success: true, user: user});
+      })
+    });
 
-        auth(req, res, next);
-    },
-    logout: function(req, res, next) {
-        req.logout();
-        res.redirect('/home');
-    },
-    isAuthenticated: function(req, res, next) {
-        if (!req.isAuthenticated()) {
-            res.status(403);
-            res.end();
-        }
-        else {
-            next();
-        }
-    },
-    isInRole: function(role) {
-        return function(req, res, next) {
-            if (req.isAuthenticated() && req.user.roles.indexOf(role) > -1) {
-                next();
-            }
-            else {
-                res.status(403);
-                res.end();
-            }
-        }
+    auth(req, res, next);
+  },
+  logout: function (req, res, next) {
+    req.logout();
+    res.redirect('/home');
+  },
+  isAuthenticated: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      res.status(403);
+      res.render('not-authorized', {
+        title: 'Unauthorized'
+      });
     }
-}
+    else {
+      next();
+    }
+  },
+  isInRole: function (role) {
+    return function (req, res, next) {
+      if (req.isAuthenticated() && req.user.roles.indexOf(role) > -1) {
+        next();
+      }
+      else {
+        res.status(403);
+        res.render('not-authorized', {
+          title: 'Unauthorized'
+        });
+      }
+    }
+  }
+};
